7280f6f22da03dd66cc38b8dfa08d9bb43523637
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-06-08 10:54:20 +0100

    EA-5 Add component pages to ensure router is functioning correctly.
    Co-authored-by: glrta <to.gio@pm.me>

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/landingPage/Landing";
-// import About from "./pages/landingPage/";
-// import SignUp from "./pages/signUpPage/";
-// import Login from "./pages/loginPage/";
-// import MyMissions from "./pages/myMissionsPage/";
-// import MyProfile from "./pages/myProfilePage/";
-// import Mission from "./components/missions/getSetReadyGo";
-// import Evidence from "./components/missions/getSetReadyGo";
+import About from "./pages/aboutPage/About";
+import Login from "./pages/loginPage/Login";
+import SignUp from "./pages/signUpPage/SignUp";
+import MyMissions from "./pages/myMissionsPage/MyMissions";
+import MyProfile from "./pages/myProfilePage/MyProfile";
+import Mission from "./pages/missionPage/Mission";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={() => <Landing />} />
-        {/* <Route path="/about" component={() => <About />} />
+        <Route path="/about" component={() => <About />} />
         <Route path="/signup" component={() => <SignUp />} />
         <Route path="/login" component={() => <Login />} />
         <Route path="/my-missions" component={() => <MyMissions />} />
         <Route path="/my-profile" component={() => <MyProfile />} />
         <Route path="/mission/:number" component={() => <Mission />} />
-        <Route path="/evidence/:number" component={() => <Evidence />} /> */}
       </Switch>
     </Router>
   );
---
+++ src/pages/myMissionsPage/MyMissions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyMissions = () => {
+  return <h1>My missions page</h1>;
+};
+
+export default MyMissions;
