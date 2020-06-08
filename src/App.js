c85ac5470ef199509504b333eedcfde8b2e90ded
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-06-08 10:44:45 +0100

    EA-5 Code router functionality - need to add pages to files.
    Co-authored-by: glrta <to.gio@pm.me>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Landing from "./pages/landingPage/Landing";
+// import About from "./pages/landingPage/";
+// import SignUp from "./pages/signUpPage/";
+// import Login from "./pages/loginPage/";
+// import MyMissions from "./pages/myMissionsPage/";
+// import MyProfile from "./pages/myProfilePage/";
+// import Mission from "./components/missions/getSetReadyGo";
+// import Evidence from "./components/missions/getSetReadyGo";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={() => <Landing />} />
+        {/* <Route path="/about" component={() => <About />} />
+        <Route path="/signup" component={() => <SignUp />} />
+        <Route path="/login" component={() => <Login />} />
+        <Route path="/my-missions" component={() => <MyMissions />} />
+        <Route path="/my-profile" component={() => <MyProfile />} />
+        <Route path="/mission/:number" component={() => <Mission />} />
+        <Route path="/evidence/:number" component={() => <Evidence />} /> */}
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
