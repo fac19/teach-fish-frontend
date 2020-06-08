6b82ec349592fb90e8c9cca48c85e208f3fd61e8
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-06-08 12:03:31 +0100

    EA-8 Create link component. Create colour scheme file.
    Co-authored-by: glrta <to.gio@pm.me>

---
+++ src/components/global/colours.js
@@ -0,0 +1,7 @@
+const bgColour = "white";
+const fontColour = "navy";
+const primaryColour = "red";
+const secondaryColour = "pink";
+const accentColour = "orange";
+
+export { bgColour, fontColour, primaryColour, secondaryColour, accentColour };
---
+++ src/components/global/links/Links.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import {
+  bgColour,
+  fontColour,
+  primaryColour,
+  secondaryColour,
+  accentColour,
+} from "../colours";
+
+const Link = styled.a`
+  font-family: "Josefin Sans", sans-serif;
+  transition: all 0.2s ease-in-out;
+  //   color:
+  &:hover {
+    font-size: 1.2rem;
+    // color;
+  }
+`;
+
+export { Link };
