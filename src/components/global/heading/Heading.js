459d0842186dcf7aeefe5901f2627c0ae322f3d8
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-06-08 12:41:03 +0100

    Add styled heading component.
    Co-authored-by: glrta <to.gio@pm.me>

---
+++ src/components/global/heading/Heading.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import {
+  bgColour,
+  fontColour,
+  primaryColour,
+  secondaryColour,
+  accentColour,
+  linkColour,
+} from "../colours";
+
+const Heading = styled.h1`
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 2rem;
+  color: ${fontColour};
+`;
+
+export { Heading };
--- src/pages/landingPage/Landing.js
+++ src/pages/landingPage/Landing.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "../../components/global/links/Links";
+import { Heading } from "../../components/global/heading/Heading";
 
 const Landing = () => {
   return (
     <section>
-      <h1>Landing Page</h1>
+      <Heading>Landing Page</Heading>
       <Link href="/about">Learn More</Link>
     </section>
   );
