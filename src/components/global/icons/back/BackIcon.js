19bccde934d9fbdee397d3cdd7dc79f556dd6d69
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-06-09 11:23:09 +0100

    EA-4 Create back icon component
    Co-authored-by: HettieM <hettie.mcconnell@outlook.com>

---
+++ src/components/global/icons/back/BackIcon.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+import { makeStyles } from "@material-ui/core/styles";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+
+import styled from "styled-components";
+import { primaryColour } from "../../colours";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100%",
+    color: "white",
+  },
+}));
+
+const BackButton = styled.button`
+  border-radius: 100%;
+  border: none;
+  background-color: ${primaryColour};
+  width: 2.5rem;
+  height: 2.5rem;
+  cursor: pointer;
+`;
+
+const BackIcon = (props) => {
+  let history = useHistory();
+  const classes = useStyles();
+  return (
+    <BackButton
+      aria-label="Go back to previous page"
+      onClick={() => history.goBack()}>
+      <ArrowBackIcon className={classes.root} />
+    </BackButton>
+  );
+};
+
+export default BackIcon;
