564519daf04fb4f43f9243a6c926318462d9c549
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-06-15 16:54:25 +0100

    EA-30 Render loading state for mission page
    Co-authored-by: tacotoemeck <wagner.tomek@gmail.com>

--- src/pages/missionPage/Mission.js
+++ src/pages/missionPage/Mission.js
@@ -1,10 +1,11 @@
 import React from "react";
 import GetSetReadyGo from "../../components/missions/getSetReadyGo/GetSetReadyGo.js";
 import GetSet from "../../components/missions/getSet/GetSet";
+import Heading from "../../components/global/heading/Heading";
 
 const MissionForm = () => {
   // state goes here
-  const [activeStep, setActiveStep] = React.useState(0);
+  // const [activeStep, setActiveStep] = React.useState(0);
   const [currentMissionObject, setCurrentMissionObject] = React.useState({});
   const [missionState, setMissionState] = React.useState("get");
 
@@ -18,66 +19,67 @@ const MissionForm = () => {
 
       await post.json().then((data) => {
         setCurrentMissionObject(data);
-        // console.log(data)
+        console.log(data);
       });
     };
 
     func();
   }, [missionNumber]);
 
-  const token = JSON.parse(localStorage.getItem("token"));
+  // const token = JSON.parse(localStorage.getItem("token"));
 
-  const email = token ? token.email : "";
+  // const email = token ? token.email : "";
 
-  const [form, setForm] = React.useState({
-    Email: email,
-    Task2a: "",
-    Task2b: "",
-    Task2c: "",
-  });
+  // const [form, setForm] = React.useState({
+  //   Email: email,
+  //   Task2a: "",
+  //   Task2b: "",
+  //   Task2c: "",
+  // });
 
   // function for the stepper
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
+  // const handleNext = () => {
+  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  // };
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setForm({ ...form, [name]: value });
-  };
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  //   const response = await fetch(
+  //     "../../../.netlify/functions/post-entries/post-entries.js",
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "content-type": "application/json",
+  //       },
+  //       body: JSON.stringify(form),
+  //     },
+  //   );
 
-    const response = await fetch(
-      "../../../.netlify/functions/post-entries/post-entries.js",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(form),
-      },
+  //   await response.json();
+  // };
+
+  const isLoading = Object.keys(currentMissionObject).length === 0;
+
+  if (isLoading) {
+    return <Heading>Loading Mission...</Heading>;
+  } else {
+    return (
+      <>
+        <GetSetReadyGo missionState={missionState} />
+        {missionState === "get" && (
+          <GetSet
+            missionNumber={missionNumber}
+            missionName={currentMissionObject["Mission Name"]}
+            superpower={currentMissionObject.Superpower}
+            getSet={currentMissionObject["Get Set"]}
+            video={currentMissionObject.Video}
+            setMissionState={setMissionState}
+          />
+        )}
+      </>
     );
-
-    await response.json();
-  };
-
-  return (
-    <>
-      <GetSetReadyGo missionState={missionState} />
-      {missionState === "get" && (
-        <GetSet
-          missionNumber={missionNumber}
-          missionName={currentMissionObject["Mission Name"]}
-          superpower={currentMissionObject.Superpower}
-          getSet={currentMissionObject["Get Set"]}
-          video={currentMissionObject.Video}
-          setMissionState={setMissionState}
-        />
-      )}
-    </>
-  );
+  }
 };
 
 export default MissionForm;
