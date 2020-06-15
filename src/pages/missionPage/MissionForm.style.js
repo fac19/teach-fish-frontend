5cdc45b4bfa55251695b7ae79251f389d18982f4
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-06-15 13:03:01 +0100

    EA-15 Write tests
    Co-authored-by: HettieM <hettie.mcconnell@outlook.com>

--- src/pages/missionPage/MissionForm.style.js
+++ src/pages/missionPage/MissionForm.style.js
@@ -3,12 +3,7 @@ import styled from "styled-components";
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
-  /* height: 70vh; */
   justify-content: center;
-  @media (min-width: 768px) {
-    // height: 50vh;
-  }
-  border: 2px solid black;
 `;
 
 const FormInputWrapper = styled.div`
