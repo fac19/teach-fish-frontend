5c79be694edc5c180c6642dd5970059594a56974
Hannah Gooding <47333850+hannahgooding@users.noreply.github.com>
2020-06-15 10:38:09 +0100

    EA-7 Make subheading component and test
    Co-authored-by: HettieM <hettie.mcconnell@outlook.com>

---
+++ src/components/global/subheading/Subheading.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+
+import Subheading from "./Subheading";
+
+test("Heading component is styled", () => {
+  const tree = renderer.create(<Subheading />).toJSON();
+  expect(tree).toMatchSnapshot();
+  expect(tree).toHaveStyleRule("color", "#032049");
+  expect(tree).toHaveStyleRule("font-size", "1.5rem");
+  expect(tree).toHaveStyleRule("font-size", "3rem", {
+    media: "(min-width:600px)",
+  });
+});
