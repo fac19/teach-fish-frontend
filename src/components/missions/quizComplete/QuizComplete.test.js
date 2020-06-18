d3de7723d453bcc34843e73afbec6f533a8a6d0e
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-06-18 14:25:29 +0100

    EA-7 Add testing for QuizComplete page

--- src/components/missions/quizComplete/QuizComplete.test.js
+++ src/components/missions/quizComplete/QuizComplete.test.js
@@ -3,10 +3,17 @@ import { render, screen } from "@testing-library/react";
 import QuizComplete from "./QuizComplete";
 
 test("Quiz correct renders onto the page", () => {
-  const quizAnswersCorrect = true;
+  let quizAnswersCorrect = true;
 
   render(<QuizComplete quizAnswersCorrect={quizAnswersCorrect} />);
   screen.getByText("You are literally a STAR");
   screen.getByText("Go");
   //   screen.getByText("Correct Answer 1");
+
+  quizAnswersCorrect = false;
+  render(<QuizComplete quizAnswersCorrect={quizAnswersCorrect} />);
+  screen.getByText(
+    "Unfortunately that isn't correct! Please restart the mission to Get Set",
+  );
+  screen.getByText("Back to start of mission");
 });
